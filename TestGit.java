c2ab89e10c6dd2120442a9eb1350bedb14169dc2
Ahmad <humbatovahmad@gmail.com>
2023-01-28 00:26:45 -0500

    This is third line

--- TestGit.java
+++ TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[]args){
         System.out.println("Hello world");
         System.out.println("Life is beautiful");
+        System.out.println("Life is not bad");
 
     }
 
